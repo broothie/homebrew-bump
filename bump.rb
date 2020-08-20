04744466288c3fe8f406a3bf011144400d9fcc01
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-20 09:26:03 -0700

    Brew formula update for bump version 0.0.4

--- bump.rb
+++ bump.rb
@@ -2,16 +2,16 @@
 class Bump < Formula
   desc "bump - SemVer bumping made easy!"
   homepage "https://github.com/broothie/bump"
-  version "0.0.1"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/broothie/bump/releases/download/0.0.1/bump_0.0.1_darwin_amd64.tar.gz"
-    sha256 "d3180f0598dff376be8e6740faf2b2d872c74e7c35fd6868caf095ff856cc93a"
+    url "https://github.com/broothie/bump/releases/download/0.0.4/bump_0.0.4_darwin_amd64.tar.gz"
+    sha256 "0c960083fb6a68054cb61d26cc94810d8a72a6ebe3620d2aeb22cdc9b88a674d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/broothie/bump/releases/download/0.0.1/bump_0.0.1_linux_amd64.tar.gz"
-      sha256 "b470ced3eef6e5d374d24311d446961c7f41f42aacd6682a20f2e235dc3bca72"
+      url "https://github.com/broothie/bump/releases/download/0.0.4/bump_0.0.4_linux_amd64.tar.gz"
+      sha256 "622b65d1494fb76820c41562c5ae67baa3a420e35e4df46bc336471b4cc5e4eb"
     end
   end
 
